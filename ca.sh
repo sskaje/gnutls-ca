c0298bc74501697b22c559542f6c2619ee494a34
sskaje <sskaje@gmail.com>
2015-12-11 15:18:15 +0800

    add ca related commands and template examples

--- ca.sh
+++ ca.sh
@@ -39,12 +39,13 @@ GnuTLS CA Script
 Author: sskaje http://sskaje.me/
 Usage:
     /path/to/ca.sh 
-        genkey  NAME            generate private key, NAME-key.pem
+        genca   NAME            generate self signed 
+        genkey  NAME [KEYBITS]  generate private key, NAME-key.pem, default KEYBITS=2048
         genreq  NAME            generate certification request, NAME.tmpl required, NAME-cert.csr
         signreq NAME CA_NAME    create certificate from NAME-cert.csr, NAME-cert.pem
         to_p12  NAME [CA_NAME]  export to pkcs#12 NAME.p12, ca cert included if CA_NAME is valid
         p7sign  NAME CA_NAME    sign NAME.mobileconfig to NAME.signed.mobileconfig, gnutls 3.4.x+ required
-
+        gencrl  NAME CA_NAME    create CRL, NAME-crl.pem
 
 USAGE
 
@@ -64,11 +65,11 @@ fi
 if [ -z $CERTTOOL ]; then
     CERTTOOL=certtool
 fi
-echo "Using $CERTTOOL"
 
 CAPATH=
 CACERT=
 CAKEY=
+KEYBITS=2048
 
 function require_template()
 {
@@ -90,8 +91,8 @@ function to_lower()
 
 function capath()
 {
-    UPPERNAME=$(to_upper $1)
-    LOWERNAME=$(to_lower $1)
+    local UPPERNAME=$(to_upper $1)
+    local LOWERNAME=$(to_lower $1)
     CAPATH="$CAROOT/${UPPERNAME} CA/${LOWERNAME}_ca"
 }
 
@@ -117,10 +118,32 @@ function cacert()
     fi
 }
 
+function ca_load()
+{
+    cacert $1
+    cakey  $1
+}
+
+function genca()
+{
+    if [ ! -f $1-key.pem ]; then
+        echo "Run \`ca.sh genkey $1\` first"
+        exit
+    fi
+    echo $CERTTOOL --generate-self-signed --load-privkey $1-key.pem --template $1.tmpl --outfile $1-cert.pem
+    $CERTTOOL --generate-self-signed --load-privkey $1-key.pem --template $1.tmpl --outfile $1-cert.pem
+}
+
 function genkey()
 {
+    if [ $# -gt 1 ]; then
+        if [ $2 -ge 1024 ]; then
+            KEYBITS=$2
+        fi
+    fi
+
     echo $CERTTOOL --generate-privkey --outfile $1-key.pem 
-    $CERTTOOL --generate-privkey --outfile $1-key.pem 
+    $CERTTOOL --generate-privkey --bits $KEYBITS --outfile $1-key.pem 
 }
 
 function genreq()
@@ -133,9 +156,8 @@ function genreq()
 function signreq()
 {
     require_template $1
-    cacert $2
-    cakey $2
-    echo $CERTTOOL --generate-certificate --load-request $1-cert.csr --load-ca-certificate "${CACERT}" --load-ca-privkey "${CAKEY}" --template $1.tmpl --outfile $1-cert.pem
+    ca_load $2
+    echo $CERTTOOL --generate-certificate --load-request $1-cert.csr --load-ca-certificate "'${CACERT}'" --load-ca-privkey "'${CAKEY}'" --template $1.tmpl --outfile $1-cert.pem
     $CERTTOOL --generate-certificate --load-request $1-cert.csr --load-ca-certificate "${CACERT}" --load-ca-privkey "${CAKEY}" --template $1.tmpl --outfile $1-cert.pem
 }
 
@@ -143,24 +165,37 @@ function to_p12()
 {
     if [ $2 != "" ];
     then 
-        cacert $2
+        ca_load $2
 
+        echo $CERTTOOL --load-ca-certificate "'${CACERT}'" --load-certificate $1-cert.pem --load-privkey $1-key.pem --to-p12 --p12-name="$1" --outder --outfile $1.p12
         $CERTTOOL --load-ca-certificate "${CACERT}" --load-certificate $1-cert.pem --load-privkey $1-key.pem --to-p12 --p12-name="$1" --outder --outfile $1.p12
     else
+        echo $CERTTOOL --load-certificate $1-cert.pem --load-privkey $1-key.pem --to-p12 --p12-name="$1" --outder --outfile $1.p12
         $CERTTOOL --load-certificate $1-cert.pem --load-privkey $1-key.pem --to-p12 --p12-name="$1" --outder --outfile $1.p12
     fi
 }
 
 function p7sign()
 {
-	cacert $2
-	cakey $2
-	$CERTTOOL --p7-sign --load-privkey "${CAKEY}" --load-certificate "${CACERT}" --infile $1.mobileconfig --outder --outfile $1.signed.mobileconfig
+    ca_load $2
+    echo $CERTTOOL --p7-sign --load-privkey "'${CAKEY}'" --load-certificate "'${CACERT}'" --infile $1.mobileconfig --outder --outfile $1.signed.mobileconfig
+    $CERTTOOL --p7-sign --load-privkey "${CAKEY}" --load-certificate "${CACERT}" --infile $1.mobileconfig --outder --outfile $1.signed.mobileconfig
+}
+
+function gencrl()
+{
+    ca_load $2
+    echo $CERTTOOL --generate-crl --load-ca-privkey "'${CAKEY}'"  --load-ca-certificate "'${CACERT}'" --template $1.tmpl --outfile $1-crl.pem
+    $CERTTOOL --generate-crl --load-ca-privkey "${CAKEY}"  --load-ca-certificate "${CACERT}" --template $1.tmpl --outfile $1-crl.pem
 }
 
 case $1 in 
+    genca) 
+        genca $2
+        ;;
+
     genkey)
-        genkey $2
+        genkey $2 $3
         ;;
 
     genreq)
@@ -177,6 +212,10 @@ case $1 in
 
     p7sign)
         p7sign $2 $3
+        ;;
+
+    gencrl)
+        gencrl $2 $3
         ;;
 
     *)
